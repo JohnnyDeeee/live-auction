5a7f2f6195dfc6be08445dfec5ac9557d8380d1a
John Doe <gamefake@live.nl>
2025-11-05 17:07:23 +0100

    Add register and authentication using Breeze
    This will allow us to protect routes.

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
     server: {
         host: '0.0.0.0',
         hmr: {
             host: 'localhost',
-        }
+        },
     },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.jsx',
             refresh: true,
         }),
-        tailwindcss(),
+        react(),
     ],
 });
